64116f5431bece9b9f03ece27c3456643e28d957
AshenOneYe <1282496341@qq.com>
2019-06-24 15:55:42 +0800

    网页引入css

--- sdses-admin/src/main/java/io/sdses/modules/wechat/controller/WeChatWebPageCpntroller.java
+++ sdses-admin/src/main/java/io/sdses/modules/wechat/controller/WeChatWebPageCpntroller.java
@@ -1,14 +1,16 @@
 package io.sdses.modules.wechat.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/wechat/page")
-public class WeChatWebCpntroller {
-    @RequestMapping("/modules/{module}/{url}.html")
-    public String module(@PathVariable("module") String module, @PathVariable("url") String url){
-        return "modules/" + module + "/" + url;
+public class WeChatWebPageCpntroller {
+    @RequestMapping("{url}.html")
+    public String module(@PathVariable("url") String url){
+        return "/modules/wechat/" + url;
     }
+
 }
